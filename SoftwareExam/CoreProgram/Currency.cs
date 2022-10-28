769ecebf68127662bc683e9c3386049be8d95e65
Mayonnaisu <toli025@student.kristiania.no>
2022-10-28 16:45:43 +0200

    Currency

--- SoftwareExam/CoreProgram/Currency.cs
+++ SoftwareExam/CoreProgram/Currency.cs
@@ -13,8 +13,91 @@ namespace SoftwareExam.CoreProgram {
         private int _copper;
 
 
+        public void Add(Currency currency)
+        {
+            _gold += currency._gold;
+            _silver += currency._silver;
+            _copper += currency._copper;
+
+            Convert();
+        }
 
 
-    
+        public void Substract(Currency currency)
+        {
+            _gold -= currency._gold;
+            _silver -= currency._silver;
+            _copper -= currency._copper;
+
+            Convert();
+        }
+
+        public void Convert()
+        {
+            while (_copper >= 10) {
+                _silver++;
+                _copper -= 10;
+            }
+            while (_silver >= 10) {
+                _gold++;
+                _silver -= 10;
+            }
+
+            while (_copper < 0) {
+                _silver--;
+                _copper += 10;
+            }
+
+            while (_silver < 0) {
+                _gold--;
+                _silver += 10;
+            }
+
+        }
+
+        //Converts all to copper to check price.
+        private int CheckPriceInCopper(Currency currency)
+        {
+            return currency._copper + (currency._silver * 10) + (currency._gold * 100);
+        }
+
+
+        public bool GreaterThan(Currency currency)
+        {
+            int price = CheckPriceInCopper(currency);
+            int balance = CheckPriceInCopper(this);
+
+            if (price > balance) {
+                return false;
+            }
+            return true;
+        }
+
+        public int Gold
+        {
+            get
+            {
+                return _gold;
+            }
+            set
+            {
+                if (value < 0) {
+                    throw new Exception("Invalid, gold must be larger than 0");
+                }
+            }
+        }
+        public int Silver
+        {
+            get
+            {
+                return _silver;
+            }
+            set
+            {
+                if (value < 0) {
+                    throw new Exception("");
+                }
+            }
+        }
     }
 }
