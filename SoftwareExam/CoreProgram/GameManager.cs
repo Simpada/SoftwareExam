bc8df42639c3148201d647ef005b019e2104a97b
Marcus <60523971+Simpada@users.noreply.github.com>
2022-11-18 15:24:22 +0100

    Fixed parsing of adventurers and gear in load

--- SoftwareExam/CoreProgram/GameManager.cs
+++ SoftwareExam/CoreProgram/GameManager.cs
@@ -168,8 +168,8 @@ namespace SoftwareExam.CoreProgram {
 
             List<Adventurer> Adventurers = DataBaseAccess.GetAdventurers(Id);
 
-            foreach (Adventurer adventurer in Adventurers) {
-                List<int> itemCodes = DataBaseAccess.GetDecorators(adventurer.Id);
+            for (int i = 0; i < Adventurers.Count; i++) {
+                List<int> itemCodes = DataBaseAccess.GetDecorators(Adventurers[i].Id);
 
                 // Parse items and give to adventurers here
 
@@ -177,23 +177,23 @@ namespace SoftwareExam.CoreProgram {
 
                     switch (itemCode) {
                         case 100:
-                        Adventurer.AddNewItem(new BasicArmor(adventurer));
+                        Adventurer.AddNewItem(new BasicArmor(Adventurers[i]));
                         break;
                         case 200:
-                        Adventurer.AddNewItem(new BasicHat(adventurer));
+                        Adventurer.AddNewItem(new BasicHat(Adventurers[i]));
                         break;
                         case 300:
-                        Adventurer.AddNewItem(new BasicOffHand(adventurer));
+                        Adventurer.AddNewItem(new BasicOffHand(Adventurers[i]));
                         break;
                         case 400:
-                        Adventurer.AddNewItem(new BasicTrinket(adventurer));
+                        Adventurer.AddNewItem(new BasicTrinket(Adventurers[i]));
                         break;
                         case 500:
-                        Adventurer.AddNewItem(new BasicWeapon(adventurer));
+                        Adventurer.AddNewItem(new BasicWeapon(Adventurers[i]));
                         break;
                     }
-
                 }
+                Adventurers[i] = Adventurer.EquipGear(Adventurers[i]);
             }
 
             Player.Adventurers = Adventurers;
@@ -204,7 +204,6 @@ namespace SoftwareExam.CoreProgram {
         public string[] GetPlayers()
         {
             return DataBaseAccess.RetrieveAllPlayerNames();
-
         }
 
         public void DeleteSave(int saveFile) {
