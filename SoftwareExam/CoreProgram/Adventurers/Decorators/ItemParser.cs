331175da1808c25e10a753ed504d5ace95f68eb7
Marcus <60523971+Simpada@users.noreply.github.com>
2022-11-23 14:39:56 +0100

    Can parse price and items from the code

--- SoftwareExam/CoreProgram/Adventurers/Decorators/ItemParser.cs
+++ SoftwareExam/CoreProgram/Adventurers/Decorators/ItemParser.cs
@@ -48,5 +48,79 @@ namespace SoftwareExam.CoreProgram.Adventurers.Decorators {
                 _ => throw new ArgumentOutOfRangeException(id.ToString() + " is not a valid item code"),
             };
         }
+        
+        public static Currency GetItemCost(int id) {
+
+            return id switch {
+                101 => ArmorPlateArmor.Cost,
+                102 => ArmorElvenRobe.Cost,
+                103 => ArmorLeatherSuit.Cost,
+                104 => ArmorFourCloverSuit.Cost,
+                105 => ArmorOfGod.Cost,
+
+                201 => HatPlateHelmet.Cost,
+                202 => HatElvenTopHat.Cost,
+                203 => HatHarlequinCrest.Cost,
+                204 => HatJuggernautsHelmet.Cost,
+                205 => HatLeatherHat.Cost,
+
+                301 => OffHandWoodenShield.Cost,
+                302 => OffHandSteelArrows.Cost,
+                303 => OffHandBookOfWisdom.Cost,
+                304 => OffHandMythrilShield.Cost,
+                305 => OffHandGiantsShield.Cost,
+
+                401 => TrinketRabbitsFoot.Cost,
+                402 => TrinketTheChickens.Cost,
+                403 => TrinketMimir.Cost,
+                404 => TrinketRingOfPower.Cost,
+                405 => TrinketFairyDust.Cost,
+
+                501 => WeaponIronSword.Cost,
+                502 => WeaponBinosKitchenKnife.Cost,
+                503 => WeaponEldersStaff.Cost,
+                504 => WeaponOrbOfDarkness.Cost,
+                505 => WeaponWindripperBow.Cost,
+                506 => WeaponSwordOfOmens.Cost,
+                _ => throw new ArgumentOutOfRangeException(id.ToString() + " is not a valid item code"),
+            };
+        }
+        
+        public static BaseDecoratedAdventurer GetItem(int id) {
+
+            return id switch {
+                101 => new ArmorPlateArmor(new Warrior()),
+                102 => new ArmorElvenRobe(new Warrior()),
+                103 => new ArmorLeatherSuit(new Warrior()),
+                104 => new ArmorFourCloverSuit(new Warrior()),
+                105 => new ArmorOfGod(new Warrior()),
+
+                201 => new HatPlateHelmet(new Warrior()),
+                202 => new HatElvenTopHat(new Warrior()),
+                203 => new HatHarlequinCrest(new Warrior()),
+                204 => new HatJuggernautsHelmet(new Warrior()),
+                205 => new HatLeatherHat(new Warrior()),
+
+                301 => new OffHandWoodenShield(new Warrior()),
+                302 => new OffHandSteelArrows(new Warrior()),
+                303 => new OffHandBookOfWisdom(new Warrior()),
+                304 => new OffHandMythrilShield(new Warrior()),
+                305 => new OffHandGiantsShield(new Warrior()),
+
+                401 => new TrinketRabbitsFoot(new Warrior()),
+                402 => new TrinketTheChickens(new Warrior()),
+                403 => new TrinketMimir(new Warrior()),
+                404 => new TrinketRingOfPower(new Warrior()),
+                405 => new TrinketFairyDust(new Warrior()),
+
+                501 => new WeaponIronSword(new Warrior()),
+                502 => new WeaponBinosKitchenKnife(new Warrior()),
+                503 => new WeaponEldersStaff(new Warrior()),
+                504 => new WeaponOrbOfDarkness(new Warrior()),
+                505 => new WeaponWindripperBow(new Warrior()),
+                506 => new WeaponSwordOfOmens(new Warrior()),
+                _ => throw new ArgumentOutOfRangeException(id.ToString() + " is not a valid item code"),
+            };
+        }
     }
 }
