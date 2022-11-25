078211ee2a9df13694ffe9bb738bac2ca3b35160
Marcus <60523971+Simpada@users.noreply.github.com>
2022-11-25 18:10:58 +0100

    Fix adventurer factories

--- SoftwareExam/CoreProgram/Adventurers/Factory/IAdventurerFactory.cs
+++ SoftwareExam/CoreProgram/Adventurers/Factory/IAdventurerFactory.cs
@@ -1,6 +1,6 @@
 ﻿namespace SoftwareExam.CoreProgram.Adventurers.Factory {
-    internal interface IAdventurerFactory {
-
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public interface IAdventurerFactory {
         public abstract Adventurer CreateAdventurer();
 
     }
